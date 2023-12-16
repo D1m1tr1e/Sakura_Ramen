c4e30ea4a3448000b13af350221fe2ed1fb69f82
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2023-12-16 22:41:28 +0100

    finished how to order section

--- sakura/src/app/app.component.ts
+++ sakura/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landingPage/landingPage.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, HowToOrderComponent, OurMenuComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- sakura/src/app/landingPage/landingPage.component.ts
+++ sakura/src/app/landingPage/landingPage.component.ts
@@ -9,8 +9,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     imports: [CommonModule, NavbarComponent],
     template: `<section>
                   <div><app-navbar></app-navbar></div>
-                  <h1 class="fontRaleway">SAKURA RAMEN</h1>
-                  <h2 class="fontRaleway">THE BEST RAMEN IN TOWN</h2>
+                  <h1 class="fontRaleway800">SAKURA RAMEN</h1>
+                  <h2 class="fontRaleway400">THE BEST RAMEN IN TOWN</h2>
                </section>`,
     styleUrls: ['./landingPage.component.scss']
 })
