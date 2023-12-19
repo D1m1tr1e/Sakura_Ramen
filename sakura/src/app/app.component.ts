096f6e0b6eea6b67c0d3197b2184e5e38ac99341
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2023-12-19 21:43:00 +0100

    .

--- sakura/src/app/app.component.ts
+++ sakura/src/app/app.component.ts
@@ -4,11 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landingPage/landingPage.component';
 import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
+import { FindUsComponent } from './find-us/find-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, HowToOrderComponent, OurMenuComponent ],
+  imports: [CommonModule, RouterOutlet,
+            LandingPageComponent, HowToOrderComponent,
+            OurMenuComponent, FindUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
