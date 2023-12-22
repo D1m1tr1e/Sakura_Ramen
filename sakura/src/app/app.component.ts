5e9082166ac5f91066c79e65a006e086f9cbe571
D1m1tr1e <126110046+D1m1tr1e@users.noreply.github.com>
2023-12-22 22:13:17 +0100

    code restructured

--- sakura/src/app/app.component.ts
+++ sakura/src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LandingPageComponent } from './landingPage/landingPage.component';
-import { HowToOrderComponent } from './how-to-order/how-to-order.component';
-import { OurMenuComponent } from './our-menu/our-menu.component';
-import { FindUsComponent } from './find-us/find-us.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { InprintComponent } from './inprint/inprint.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet,
-            LandingPageComponent, HowToOrderComponent,
-            OurMenuComponent, FindUsComponent],
+    FooterComponent, MainContentComponent, InprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
